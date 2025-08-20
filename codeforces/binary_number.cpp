46c4cdb13f77264ee481a06225dcaf79f15775c6
Samayra312007 <samayra312007@gmail.com>
2025-08-20 22:03:55 +0530

    add binary number modified

--- codeforces/binary_number.cpp
+++ codeforces/binary_number.cpp
@@ -1,21 +1,23 @@
-#include<bits/stdc++.h>
-#include<string>
+#include <bits/stdc++.h>
+#include <string>
 
 using namespace std;
 
 int main()
 {
-    int n;
+    string n;
     cin >> n;
-    string nstr = to_string(n);
-    int decn = stoi(nstr, nullptr, 2);
+    int decn = stoi(n, nullptr, 2);
     int cnt = 0;
-    while(decn != 1){
-        if(decn%2 == 0){
+    while (decn != 1)
+    {
+        if (decn % 2 == 0)
+        {
             decn /= 2;
             cnt++;
         }
-        else{
+        else
+        {
             decn += 1;
             cnt++;
         }
