52abb90d29a5d9ee1d86116ada4768b95f1689d6
Samayra312007 <samayra312007@gmail.com>
2025-08-20 21:57:50 +0530

    add binary number problem

---
+++ codeforces/binary_number.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    string nstr = to_string(n);
+    int decn = stoi(nstr, nullptr, 2);
+    int cnt = 0;
+    while(decn != 1){
+        if(decn%2 == 0){
+            decn /= 2;
+            cnt++;
+        }
+        else{
+            decn += 1;
+            cnt++;
+        }
+    }
+    cout << cnt << endl;
+}
