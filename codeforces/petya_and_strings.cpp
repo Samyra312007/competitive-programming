209e1b35ac5daf5a7e6934c9b307ef8f510b8111
Samayra312007 <samayra312007@gmail.com>
2025-08-22 21:58:10 +0530

    add petya and strings problem

---
+++ codeforces/petya_and_strings.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s1, s2;
+    cin >> s1 >> s2;
+    for (auto &x : s1)
+    {
+        x = tolower(x);
+    }
+    for (auto &x : s2)
+    {
+        x = tolower(x);
+    }
+    int result = s1.compare(s2);
+    if (result < 0)
+    {
+        cout << -1 << endl;
+    }
+    else if (result > 0)
+    {
+        cout << '1' << endl;
+    }
+    else
+        cout << '0' << endl;
+
+    return 0;
+}
